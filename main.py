5abe7c53ad1a7a182e569c2c9cafee994d62ee5f
tyshenkoalinko <108027114+tyshenkoalinko@users.noreply.github.com>
2022-06-23 01:58:06 +0500

    update code
    update

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ def ArrayChange():
     chars=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'g', 'k', 'l', 'm', 'n', 'o', 'p']
     newmas=[]
     for i in range(10):
-        mas.append(rnd.sample(chars, rnd.randint(1, 5)))
+        k=rnd.randint(1, 5)
+        s=""
+        for j in range(k):
+            s+=rnd.sample(chars, k)[j]
+            mas.append(s)
     for i in range(10):
         if(len(mas[i])<=3):
             newmas.append(mas[i])
