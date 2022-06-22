ca4aacd31d6a2fa3809311a6f2739c5b6aa4a578
tyshenkoalinko <108027114+tyshenkoalinko@users.noreply.github.com>
2022-06-23 01:42:30 +0500

    add chars for chars array
    add chars for chars array

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random as rnd
 def ArrayChange():
     mas=[]
-    chars=['a', 'b', 'c']
+    chars=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'g', 'k', 'l', 'm', 'n', 'o', 'p']
     newmas=[]
     for i in range(10):
         mas.append(rnd.sample(chars, rnd.randint(1, 5)))
